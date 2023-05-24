ebe6826c5e8ebf07cf58c0e22c9512282f42b4a6
Kalle Päplow <paeplow@paulsen-online.de>
2023-05-24 10:44:13 +0200

    Fix: added catch block to api

--- src/api/get_products.ts
+++ src/api/get_products.ts
@@ -1,7 +1,11 @@
 import Product from "../types/product";
 
-export function getProducts(callback: (products: [Product]) => void) {
+export function getProducts(callback: (products: Product[]) => void) {
     fetch('https://fakestoreapi.com/products')
             .then(res=>res.json())
             .then(json=> callback(json))
+            .catch(error => {
+                callback([])
+                console.error('parsing failed', error)
+            })
 }
